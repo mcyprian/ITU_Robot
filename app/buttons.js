9390bff0a5327851621d0eb2e2629f9beedd31b9
Michal Cyprian <mcyprian@redhat.com>
2016-12-10 22:43:59 +0100

    Add brake and gear progress bars

--- app/buttons.js
+++ app/buttons.js
@@ -2,3 +2,11 @@ function toggleHandling() {
     $('button.toggle').toggleClass('btn-success');
     $('button.toggle').toggleClass('btn-default');
 }
+
+function button_on(type) {
+    $(type).css('width', '100%');
+}
+
+function button_off(type) {
+    $(type).css('width', '0%');
+}
