bf46567386c93d92496b27dbe3fde0728dc881e0
Michal Cyprian <mcyprian@redhat.com>
2016-12-11 11:39:37 +0100

    Add device orientation arrows, glyphicons fonts

--- app/buttons.js
+++ app/buttons.js
@@ -3,10 +3,23 @@ function toggleHandling() {
     $('button.toggle').toggleClass('btn-default');
 }
 
+function button_on_slow(type) {
+    $(type).css({ "pointer-events":"none"});
+    $(type).addClass('active');
+    $(type).animate({width: "100%"}, 600, function() {
+        $(type).css({ "pointer-events":"auto"});
+        if (!$(type).hasClass('active')) {
+            $(type).css('width', '0%');
+        }
+    });
+}
+
 function button_on(type) {
     $(type).css('width', '100%');
+    $(type).addClass('active');
 }
 
 function button_off(type) {
     $(type).css('width', '0%');
+    $(type).removeClass('active');
 }
--- app/orientation.js
+++ app/orientation.js
@@ -18,4 +18,20 @@ function printOrientation() {
         outputEl.innerHTML = '<b><br>Orientation:</b>'
            + ' LR: ' + lrVal + ' FB: ' + titleFB + ' direction: ' + direction
     }, 1/3000);
+
+    setInterval(function() {
+        var temp = Math.floor(titleFB);
+        var direction = temp > 0 ? "left" : "right";
+        var opposite = direction == "left" ? "right" : "left";
+        var temp = Math.abs(temp);
+        for (var i = 0; i <= 7; i++) {
+            var num = direction == "right" ? i : 7 - i;
+            if (temp > 3 && i * 11 <= temp) {
+                $('span.' + direction + '-arr' + num).css('color', 'cyan');
+            } else {
+                $('span.' + direction + '-arr' + num).css('color', '#4e5d6c');
+            }
+            $('span.' + opposite + '-arr' + i).css('color', '#4e5d6c');
+        }
+    }, 1/3000);
 }
